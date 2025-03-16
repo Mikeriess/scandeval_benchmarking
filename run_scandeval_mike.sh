a1391a8a3446bc6e8c3b0e68cee443d4763c0b1a
Mike Riess <mike@riess.no>
2025-03-16 10:17:54 +0100

    --only-allow-safetensors

--- run_scandeval_mike.sh
+++ run_scandeval_mike.sh
@@ -2,12 +2,12 @@ pip install -U euroeval[all]
 
 while true; do
 
-euroeval --evaluate-test-split --clear-model-cache --trust-remote-code -l fr -l it -m meta-llama/Llama-3.1-70B-Instruct
-euroeval --evaluate-test-split --clear-model-cache --trust-remote-code -l fr -l it -m Qwen/Qwen2.5-72B-Instruct
-euroeval --evaluate-test-split --clear-model-cache --trust-remote-code -l fr -l it -m Qwen/QwQ-32B-Preview
-euroeval --evaluate-test-split --clear-model-cache --trust-remote-code -l fr -l it -m ThatsGroes/gemma-2-27b-it-FP8-Dynamic
-euroeval --evaluate-test-split --clear-model-cache --trust-remote-code -l fr -l it -m mistralai/Mixtral-8x7B-v0.1
-euroeval --evaluate-test-split --clear-model-cache --trust-remote-code -l fr -l it -m mistralai/Mixtral-8x7B-Instruct-v0.1
-euroeval --evaluate-test-split --clear-model-cache --trust-remote-code -l fr -l it -l fo -l is -m google/gemma-2-27b-it
+euroeval --evaluate-test-split --clear-model-cache --trust-remote-code --only-allow-safetensors -l fr -l it -m meta-llama/Llama-3.1-70B-Instruct
+euroeval --evaluate-test-split --clear-model-cache --trust-remote-code --only-allow-safetensors -l fr -l it -m Qwen/Qwen2.5-72B-Instruct
+euroeval --evaluate-test-split --clear-model-cache --trust-remote-code --only-allow-safetensors -l fr -l it -m Qwen/QwQ-32B-Preview
+euroeval --evaluate-test-split --clear-model-cache --trust-remote-code --only-allow-safetensors -l fr -l it -m ThatsGroes/gemma-2-27b-it-FP8-Dynamic
+euroeval --evaluate-test-split --clear-model-cache --trust-remote-code --only-allow-safetensors -l fr -l it -m mistralai/Mixtral-8x7B-v0.1
+euroeval --evaluate-test-split --clear-model-cache --trust-remote-code --only-allow-safetensors -l fr -l it -m mistralai/Mixtral-8x7B-Instruct-v0.1
+euroeval --evaluate-test-split --clear-model-cache --trust-remote-code --only-allow-safetensors -l fr -l it -l fo -l is -m google/gemma-2-27b-it
 
 done
